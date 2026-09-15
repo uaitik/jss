c7ded408328d68721cb541df7441d99e6be6a2b5
uaitik <uaiti.kk@gmail.com>
2026-09-15 23:52:23 +0200

    Add files via upload

---
+++ Hrechka_zewnetrzny.js
@@ -0,0 +1,5 @@
+document.write("Mój numer w dzienniku to 5".fontcolor("red").bold().fontsize(7)+"<br>");
+document.write("Pierwszy wiersz to: 12 Nie Tak 7".fontcolor("red").bold().fontsize(7)+"<br>");
+document.write("Drugi wiersz to: 12 Nie Tak 7".fontcolor("red").bold().fontsize(7)+"<br>");
+document.write("ostatnia modyfikacja strony".strike().fontcolor("yellow").fontsize(7)+"<br>");
+document.write(document.lastModified.strike().fontcolor("yellow").fontsize(7)+"<br>");
